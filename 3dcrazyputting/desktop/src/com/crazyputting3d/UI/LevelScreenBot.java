5f72398851f4838aee6b42dca0f13fcc4448348a
Gabrijel Radovcic <g.radovcic@student.maastrichtuniversity.nl>
2022-06-17 16:13:36 +0200

    a

--- 3dcrazyputting/desktop/src/com/crazyputting3d/UI/LevelScreenBot.java
+++ 3dcrazyputting/desktop/src/com/crazyputting3d/UI/LevelScreenBot.java
@@ -14,10 +14,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.crazyputting3d.Engine.physicsEngine;
-import com.crazyputting3d.InputReader.FunctionReader;
-
-import java.io.IOException;
-
 public class LevelScreenBot implements Screen {
     private Stage stage;
     private TextButton backButton;

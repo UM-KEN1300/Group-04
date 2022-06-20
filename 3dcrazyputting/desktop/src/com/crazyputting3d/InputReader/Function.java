d01c72a0da082554a317b5e6207f2b4799862e2d
ArjenvanGelder <arjenvangelder99@gmail.com>
2022-06-20 16:18:47 +0200

    levels

--- 3dcrazyputting/desktop/src/com/crazyputting3d/InputReader/Function.java
+++ 3dcrazyputting/desktop/src/com/crazyputting3d/InputReader/Function.java
@@ -4,9 +4,9 @@ public double getHeightFunction(int level,double x, double y){
 switch (level){
 case 1: return 0;
 case 2: return 0.4*(0.9-Math.exp(-(Math.pow(x,2)+Math.pow(y,2))/8));
-case 3: return 0.35*Math.sin(x)+0.45*Math.cos(y);
+case 3: return 0.4*(0.9-Math.exp(-(Math.pow(x,2)+Math.pow(y,2))/8));
 case 4: return Math.cos(Math.cos(x*y/3)-8);
-case 5: return 0.4*(0.9-Math.exp(-(Math.pow(x,2)+Math.pow(y,2))/8));
+case 5: return 0.35*Math.sin(x)+0.45*Math.cos(y);
 case 6: return 0.4*(0.9-Math.exp(-0.2*(Math.pow(x,2)+Math.pow(y,2))/8));
 case 7: return 0.4*(0.9-Math.exp(-(Math.pow(x,2)+Math.pow(y,2))/8));
 case 8: return 0;
